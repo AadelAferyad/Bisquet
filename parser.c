dc49f424a35467de7c7a84c4ca387eb7700862c7
AadelAferyad <animevineu@gmail.com>
2024-09-14 04:10:59 +0100

    fix

--- parser.c
+++ parser.c
@@ -10,7 +10,6 @@ void	fill_map_fs_line(char *buffer, mp *map)
 	map->empty = buffer[len - 2];/*12014*/
 	buffer[len - 2] = '.';
 	map->columns = ft_atoi(buffer);
-
 }
 
 int	validate_map_fs_line(char *buffer, mp *map)
@@ -25,11 +24,13 @@ int	validate_map_fs_line(char *buffer, mp *map)
 		if (i < 3 && (buffer[len] <= 32 || buffer[len] > 126))
 		{
 			printf("[invalid map] :The provided characters for (empty, obstacles and full) are non printable characters\n");
+			free(map);
 			return (0);
 		}
 		else if (i >= 3 && (len > 4 || !(buffer[len] >= '0' && buffer[len] <= '9'))) 	
 		{
 			printf("[Invalid map] :The provided number is invalid");
+			free(map);
 			return (0);
 		}
 		len--;
@@ -47,7 +48,10 @@ mp	*read_map_fs_line(int fd)
 
 	i = 0;
 	if (read(fd, buffer, READ_SIZE) == -1)
+	{
 		printf("failed to read the file");
+		return (NULL);
+	}
 	map = malloc(sizeof(mp));
 	if (!map)
 		return (NULL);
@@ -64,17 +68,45 @@ mp	*read_map_fs_line(int fd)
 		return (NULL);
 	return (map);
 }
+int	validate_map(int fd, mp *map)
+{
+	char	buffer[READ_SIZE];
+	int	rbytes;
+
+	lseek(fd, 0, SEEK_SET);
+	do
+	{
+
+		rbytes = read(fd, buffer, READ_SIZE);
+	} while (rbytes > 0)
+}
+
+char	**allocate_map(int fd, mp *map)
+{
+	char	**map;
+
+	if (!validate_map(fd, map))
+		return (NULL);
+	printf("%s %d tests\n", buffer, ft_strlen(buffer));
+	return (NULL);
+}
+
 mp	*parcer(char *file_name)
 {
 	int	fd;
-	int	rbytes;
 	int	i;
+	mp	*map;
 
 	i = 0;
 	fd = open(file_name, O_RDONLY);
 	if (fd == -1)
+	{
 		printf("error opening the file\n");
-	read_map_fs_line(fd);
-	
-	return (NULL);
+		return (NULL);
+	}
+	map = read_map_fs_line(fd);
+	if (!map)
+		return (NULL);
+	map->av = allocate_map(fd);	
+	return (map);
 }
